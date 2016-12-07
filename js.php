89eedc583f649d24223448adf56a9bbc009b2a90
echofb <xing_tao77@126.com>
2016-12-07 22:57:20 +0800

    测试数据第一版本

---
+++ js.php
@@ -0,0 +1,21 @@
+<?php 
+echo 13;
+ ?>
+<script src="./jquery.min.js"></script>
+<script>
+
+
+$(function(){
+
+	alert(222);
+})
+
+</script>
+ <html>
+ <head>
+ 	<title>测试文件</title>
+ </head>
+ <body>
+ 
+ </body>
+ </html>
